1abbae8908acde7d6f8625b5d0c74102c4c083b9
Mark Gallant <markgallant01@gmail.com>
2021-05-05 06:00:47 -0400

    Start writing script

---
+++ setup_script.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# This script will attempt to setup and configure
+# the system by pulling dotfiles from github, setting
+# appropriate symlinks, and running preliminary software.
+#
+# This script should be run after successfully running
+# install_script.sh
+
+# Before running, navigate to github in a web browser
+# and log in. Prepare to add a new ssh key.
+
+# copy ssh key to clipboard
+xclip -selection clipboard < ~/.ssh/id_ed25519.pub
+
+# wait until ssh key has been added to github
+echo "SSH key copied to clipboard. Add to github and press enter when
+finished..."
+read
+
+echo "done\n"
+
