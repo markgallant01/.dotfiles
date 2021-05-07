d819d30007e34b74a1c0d702858715f75723b3fd
Mark Gallant <markgallant01@gmail.com>
2021-05-07 06:13:25 -0400

    Clean up Wine section and combine all packages

--- install_script.sh
+++ install_script.sh
@@ -46,11 +46,7 @@ packages+=(ranger rofi firefox chromium discord network-manager-applet)
 # gaming stuff
 packages+=(steam lutris)
 
-# install everything so far
-pacman -Syu ${packages[@]}
-
 # graphics drivers
-gpuDrivers=( )
 
 # detect GPU
 gpuString=$( lspci -v | grep -A1 -e VGA -e 3D | tr '[:upper:]' '[:lower:]' )
@@ -58,33 +54,39 @@ gpuString=$( lspci -v | grep -A1 -e VGA -e 3D | tr '[:upper:]' '[:lower:]' )
 # add appropriate driver packages
 if [[ ${gpuString} == *"nvidia"* ]]
 then
-    gpuDrivers+=(nvidia nvidia-utils lib32-nvidia-utils vulkan-icd-loader
+    packages+=(nvidia nvidia-utils lib32-nvidia-utils vulkan-icd-loader
     lib32-vulkan-icd-loader)
 fi
 
 if [[ ${gpuString} == *"intel"* ]]
 then
-    gpuDrivers+=(mesa lib32-mesa vulkan-intel lib32-vulkan-intel
+    packages+=(mesa lib32-mesa vulkan-intel lib32-vulkan-intel
     vulkan-icd-loader lib32-vulkan-icd-loader)
 fi
 
-# install GPU driver packages
-pacman -Syu ${gpuDrivers[@]}
+# wine and wine dependencies
+packages+=(wine-staging giflib lib32-giflib libpng lib32-libpng libldap
+lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal
+v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error
+lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib
+libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite
+lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama
+ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt
+lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs
+lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+wine-gecko wine-mono)
 
-# wine stuff (clean this up later)
-pacman -Syu wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
-
-# more wine stuff
-pacman -Syu wine-gecko wine-mono
+# install all packages
+pacman -Syu ${packages[@]}
 
 # configure git
-git config --global user.name "Mark Gallant"
-git config --global user.email "markgallant01@gmail.com"
+#git config --global user.name "Mark Gallant"
+#git config --global user.email "markgallant01@gmail.com"
 
 # generate a new ssh key
-ssh-keygen -t ed25519 =C "markgallant01@gmail.com"
+#ssh-keygen -t ed25519 =C "markgallant01@gmail.com"
 
 # start ssh agent and add new key
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+#eval "$(ssh-agent -s)"
+#ssh-add ~/.ssh/id_ed25519
 
