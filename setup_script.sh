0b02fc98474b7337d7f0a147155c5c662e170a2a
Mark Gallant <markgallant01@gmail.com>
2021-05-07 06:20:44 -0400

    Remove git stuff

--- setup_script.sh
+++ setup_script.sh
@@ -1,25 +1,10 @@
 #!/usr/bin/bash
 
 # This script will attempt to setup and configure
-# the system by pulling dotfiles from github, setting
-# appropriate symlinks, and running preliminary software.
+# the system by creating symlinks and config files.
 #
 # This script should be run after successfully running
 # install_script.sh
-
-# Before running, navigate to github in a web browser
-# and log in. Prepare to add a new ssh key.
-
-# copy ssh key to clipboard
-xclip -selection clipboard < ~/.ssh/id_ed25519.pub
-
-# wait until ssh key has been added to github
-echo "SSH key copied to clipboard. Add to github and press enter when
-finished..."
-read
-
-# pull dotfile directory from github
-git clone git@github.com:markgallant01/.dotfiles
 
 # create symlinks for dotfiles
 
