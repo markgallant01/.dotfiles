ecb6c286575b5bbdb0c1c464116fbf441021bf71
markgallant01 <markgallant01@gmail.com>
2023-01-14 18:55:34 -0500

    comment out wallpaper setter because its handled by feh

--- rc.lua
+++ rc.lua
@@ -162,11 +162,11 @@ local function set_wallpaper(s)
 end
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
+-- screen.connect_signal("property::geometry", set_wallpaper)
 
 awful.screen.connect_for_each_screen(function(s)
     -- Wallpaper
-    set_wallpaper(s)
+    -- set_wallpaper(s)
 
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
