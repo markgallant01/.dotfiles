cf4568cbc0c3337b80fea0e083a73c2b9e8c9e9e
markgallant01 <markgallant01@gmail.com>
2023-01-18 17:56:03 -0500

    fix file path for theme

--- rc.lua
+++ rc.lua
@@ -45,7 +45,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("home/mark/.config/awesome/theme.lua")
+beautiful.init("/home/mark/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
