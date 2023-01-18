e5d5060e8b53d1bae3863e4efcb47895b1ca32f2
markgallant01 <markgallant01@gmail.com>
2023-01-18 16:53:41 -0500

    comment out 'notify' import to hopefully disable notification popups

--- rc.lua
+++ rc.lua
@@ -11,7 +11,7 @@ local wibox = require("wibox")
 -- Theme handling library
 local beautiful = require("beautiful")
 -- Notification library
-local naughty = require("naughty")
+-- local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
