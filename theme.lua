e720562c2de7b6176885b48f8fded9b52f684a46
markgallant01 <markgallant01@gmail.com>
2023-01-20 21:20:27 -0500

    modify colors to make things more readable

--- theme.lua
+++ theme.lua
@@ -17,9 +17,10 @@ theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
+theme.bg_systray    = theme.bg_focus
 
-theme.fg_normal     = "#aaaaaa"
+--theme.fg_normal     = "#aaaaaa"
+theme.fg_normal     = "#ffffff"
 theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
