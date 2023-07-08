1ebf04fb27df7305256e83aa11fb9b3fe787f091
markgallant01 <markgallant01@gmail.com>
2023-07-08 15:52:45 -0400

    remove all plugins and start fresh

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,31 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- ]]
+-- Set <space> as the leader key
+-- See ':help mapleader'
+-- NOTE: Must happen before plugins are required (otherwise wrong
+-- leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Install package manager
+-- https://github.com/folke/lazy.nvim
+-- ':help lazy.nvim.txt' for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+-- You can configure plugins using the 'config' key.
+-- You can also configure plugins after the setup call,
+-- as they will be available in your neovim runtime.
+require('lazy').setup({
+})
+
