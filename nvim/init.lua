528742b2b508843c5f8cb06409f4edf23a2b0e94
markgallant01 <markgallant01@gmail.com>
2023-07-08 19:07:08 -0400

    configure mason

--- nvim/init.lua
+++ nvim/init.lua
@@ -322,3 +322,24 @@ local servers = {
   },
 }
 
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    require('lspconfig')[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+    }
+  end,
+}
+
