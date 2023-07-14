6d7f04740d00247bf6418221a7a5b22f02d67d6c
markgallant01 <markgallant01@gmail.com>
2023-07-14 19:12:38 -0400

    make ssh config file and add symlink to setup script

--- scripts/final_setup.sh
+++ scripts/final_setup.sh
@@ -58,6 +58,7 @@ ln -sf ~/.dotfiles/.xinitrc ~/.xinitrc
 ln -sf ~/.dotfiles/picom.conf ~/.config/picom.conf
 ln -sf ~/.dotfiles/.fehbg ~/.fehbg
 ln -sf ~/.dotfiles/nvim/ ~/.config/nvim
+ln -sf ~/.dotfiles/ssh_config ~/.ssh/config
 
 # copy conf files to appropriate folders
 sudo cp ~/.dotfiles/conf_files/00-input-devices.conf /etc/X11/xorg.conf.d/
