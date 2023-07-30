8fe05ae2fcc08c0181c8653d52ff17ef03250e22
markgallant01 <markgallant01@gmail.com>
2023-07-30 04:14:03 -0400

    clean up formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -306,16 +306,16 @@ require('nvim-treesitter.configs').setup {
 }
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, 
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,
   { desc = 'Go to previous diagnostic message' })
 
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, 
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next,
   { desc = 'Go to next diagnostic message' })
 
-vim.keymap.set('n', '<leader>w', vim.diagnostic.open_float, 
+vim.keymap.set('n', '<leader>w', vim.diagnostic.open_float,
   { desc = 'Open floating diagnostic message' })
 
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, 
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist,
   { desc = 'Open diagnostics list' })
 
 -- [[ Configure LSP ]]
