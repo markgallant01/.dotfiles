c7c59a531494eb2bf062465f5b741b69d4245938
markgallant01 <markgallant01@gmail.com>
2023-08-02 15:29:43 -0400

    enable onedark colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -60,7 +60,8 @@ require('lazy').setup({
   },
 
   {
-    -- Monokai theme
+    -- colorschemes
+    'navarasu/onedark.nvim',
     'loctvl842/monokai-pro.nvim',
     priority = 1000,
   },
@@ -74,7 +75,7 @@ require('lazy').setup({
         icons_enabled = false,
         component_separators = '|',
         section_separators = '',
-        theme = 'monokai-pro',
+        theme = 'onedark',
       },
     },
   },
@@ -189,55 +190,48 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string,
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep,
   { desc = '[S]earch by [G]rep' })
 
--- [[ Configure Monokai Colorscheme ]]
+-- [[ Configure Colorscheme ]]
 --
-require("monokai-pro").setup({
-  transparent_background = false,
-  terminal_colors = true,
-  devicons = true, -- highlight the icons of `nvim-web-devicons`
-  styles = {
-    comment = { italic = true },
-    keyword = { italic = false }, -- any other keyword
-    type = { italic = false }, -- (preferred) int, long, char, etc
-    storageclass = { italic = false }, -- static, register, volatile, etc
-    structure = { italic = false }, -- struct, union, enum, etc
-    parameter = { italic = true }, -- parameter pass in function
-    annotation = { italic = true },
-    tag_attribute = { italic = true }, -- attribute of tag in reactjs
-  },
-  filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-  -- Enable this will disable filter option
-  day_night = {
-    enable = false, -- turn off by default
-    day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-    night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-  },
-  inc_search = "background", -- underline | background
-  background_clear = {
-    -- "float_win",
-    "toggleterm",
-    "telescope",
-    -- "which-key",
-    "renamer",
-    "notify",
-    -- "nvim-tree",
-    -- "neo-tree",
-    -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-  },-- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
-  plugins = {
-    bufferline = {
-      underline_selected = false,
-      underline_visible = false,
+require('onedark').setup  {
+    -- Main options --
+    style = 'cool', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = false,  -- Show/hide background
+    term_colors = true, -- Change terminal color as per the selected theme style
+    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+    -- toggle theme style ---
+    toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+    toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+
+    -- Change code style ---
+    -- Options are italic, bold, underline, none
+    -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+    code_style = {
+        comments = 'italic',
+        keywords = 'none',
+        functions = 'none',
+        strings = 'none',
+        variables = 'none'
     },
-    indent_blankline = {
-      context_highlight = "default", -- default | pro
-      context_start_underline = false,
+
+    -- Lualine options --
+    lualine = {
+        transparent = false, -- lualine center bar transparency
     },
-  },
-  ---@param c Colorscheme
-  override = function(c) end,
-})
-vim.cmd([[colorscheme monokai-pro]])
+
+    -- Custom Highlights --
+    colors = {}, -- Override default colors
+    highlights = {}, -- Override highlight groups
+
+    -- Plugins Config --
+    diagnostics = {
+        darker = true, -- darker colors for diagnostic
+        undercurl = true,   -- use undercurl instead of underline for diagnostics
+        background = true,    -- use background color for virtual text
+    },
+}
+require('onedark').load()
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
