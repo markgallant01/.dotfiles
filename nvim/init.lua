6d92e553cd9cc1f967c92b29ea8d766e8219161e
markgallant01 <markgallant01@gmail.com>
2024-01-13 11:58:24 -0500

    increase tab width to 4

--- nvim/init.lua
+++ nvim/init.lua
@@ -133,9 +133,9 @@ vim.o.clipboard = 'unnamedplus'
 vim.wo.nu = true
 vim.o.relativenumber = true
 
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 vim.o.smartindent = true
