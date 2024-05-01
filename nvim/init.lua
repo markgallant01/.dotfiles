86d68cad574984b7f0268e4d9b544bb2f17511f9
markgallant01 <markgallant01@gmail.com>
2024-05-01 17:45:11 -0400

    disable italic comments in colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -216,7 +216,7 @@ require("solarized-osaka").setup({
   styles = {
     -- Style to be applied to different syntax groups
     -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
+    comments = { italic = false },
     keywords = { italic = false },
     functions = {},
     variables = {},
