cc45d45f2a69daede0e5b7ef011a31fe57bfa934
Mark Gallant <markgallant01@gmail.com>
2024-06-13 12:00:11 -0400

    clean up unused programs

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -12,41 +12,33 @@ declare -a packages=()
 packages+=("git" "openssh")
 
 # display server and some x-tools
-packages+=("xorg-server" "xorg-xsetroot" "xorg-xbacklight")
-packages+=("xorg-xinit" "xorg-xrandr")
+packages+=("xorg-server" "xorg-xinit" "xorg-xrandr")
 
-# window manager dependencies
-packages+=("libxft" "libxinerama" "ttf-dejavu-nerd")
+# development tools
+packages+=("base-devel" "clang" "gdb" "npm")
 
-# development tools for building from source
-packages+=("base-devel" "clang" "gdb")
-
-# image viewing and processing tools
-packages+=("feh")
+# media viewing tools
+packages+=("feh" "vlc")
 
 # misc utilities
 packages+=("network-manager-applet" "udisks2" "fastfetch" "zip" "unzip")
-packages+=("scrot" "cmus" "celluloid" "neovim" "dmenu" "xdg-user-dirs")
-packages+=("xclip" "npm" "wget" "curl")
+packages+=("scrot" "neovim" "xdg-user-dirs" "xclip" "wget" "curl")
 
 # file browser
 packages+=("thunar")
 
 # sound system
-packages+=("pipewire" "lib32-pipewire" "wireplumber" "pipewire-audio" "pipewire-pulse")
-packages+=("blueman" "sof-firmware")
+packages+=("pipewire" "lib32-pipewire" "pipewire-audio" "pipewire-pulse")
+packages+=("blueman" "wireplumber")
 
 # hardware monitoring
 packages+=("htop")
 
-# music downloader
+# video / music downloader
 packages+=("yt-dlp")
 
-# ebooks
-packages+=("calibre")
-
 # web browsers
-packages+=("firefox")
+packages+=("firefox" "chromium")
 
 # communication
 packages+=("discord")
@@ -54,43 +46,25 @@ packages+=("discord")
 # game launchers
 packages+=("steam" "lutris")
 
-# grab the computer's host name
-hostname=$( cat /etc/hostname )
+# video drivers depend on GPU manufacturer:
+# nvidia:
+packages+=("nvidia" "nvidia-utils" "lib32-nvidia-utils")
+packages+=("nvidia-settings")
 
-# pc-specific packages go here
-# desktop-NVIDIA GPU
-if [ "$hostname" == "archDesk" ]
-then
-  packages+=("nvidia" "nvidia-utils" "lib32-nvidia-utils")
-  packages+=("nvidia-settings")
-fi
+# intel iGPU:
+packages+=("mesa-amber" "lib32-mesa-amber" "vulkan-intel")
+packages+=("lib32-vulkan-intel")
 
-# laptop-intel iGPU
-if [ "$hostname" == "archt450s" ]
-then
-  packages+=("mesa-amber" "lib32-mesa-amber" "vulkan-intel")
-  packages+=("lib32-vulkan-intel")
-fi
+# AMD CPU GPU?
+
+# window manager
+packages+=("awesome")
 
 # install all the compiled packages
 sudo pacman -S --needed "${packages[@]}"
 
 # ensure home directory
 cd ~
-
-# download dwm and st
-git clone https://github.com/markgallant01/dwm
-git clone https://github.com/markgallant01/st
-
-# build and install dwm and st
-cd dwm
-make
-sudo make clean install
-cd
-cd st
-make
-sudo make clean install
-cd
 
 # pull temporary xinit file for the first boot
 curl -LJO https://github.com/markgallant01/.dotfiles/raw/main/.xinitrc
