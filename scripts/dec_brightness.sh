4e6d280c606c0fe6480782655a039adb11fa421d
markgallant01 <markgallant01@gmail.com>
2024-07-12 02:32:32 -0400

    create scripts for brightness control

---
+++ scripts/dec_brightness.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+brightness=$(( $brightness - 6400 ))
+echo $brightness > /sys/class/backlight/intel_backlight/brightness
+
