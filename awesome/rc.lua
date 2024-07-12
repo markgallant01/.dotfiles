30761c3d1572a9bf9bb2f3502eb8327f69644032
markgallant01 <markgallant01@gmail.com>
2024-07-12 02:12:55 -0400

    disable mouse bindings

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -221,11 +221,13 @@ end)
 -- }}}
 
 -- {{{ Mouse bindings
+--[[
 root.buttons(gears.table.join(
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
+--]]
 -- }}}
 
 -- {{{ Key bindings
