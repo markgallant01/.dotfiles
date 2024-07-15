845b8bd192d0a5825a08b43da68954da20e7a98a
markgallant01 <markgallant01@gmail.com>
2024-07-15 02:32:29 -0400

    create file

---
+++ scripts/setup_screens.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# this script uses xrandr to set up the screen settings. Uncomment
+# whichever one works and modify it to suit your needs or use xrandr's
+# output to write your own
+
+# desktop
+# xrandr --output DP-4 --primary --mode 2560x1440 --rate 120.00 \
+#    --output DP-0 --mode 2560x1440 --rate 120.00 --right-of DP-4 &
+
+# xps13 laptop
+# xrandr --output eDP-1 --mode 1920x1200 --rate 59.95
+
