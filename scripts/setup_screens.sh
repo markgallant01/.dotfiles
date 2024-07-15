99d2a14bea83a570026ddc359618819e6453d0f3
markgallant01 <markgallant01@gmail.com>
2024-07-15 02:35:51 -0400

    fix desktop call

--- scripts/setup_screens.sh
+++ scripts/setup_screens.sh
@@ -5,7 +5,7 @@
 
 # desktop
 # xrandr --output DP-4 --primary --mode 2560x1440 --rate 120.00 \
-#    --output DP-0 --mode 2560x1440 --rate 120.00 --right-of DP-4 &
+#    --output DP-0 --mode 2560x1440 --rate 120.00 --right-of DP-4
 
 # xps13 laptop
 # xrandr --output eDP-1 --mode 1920x1200 --rate 59.95
