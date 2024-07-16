ec3ca6896db2ec9eae2107f326f1461d710dbad6
markgallant01 <markgallant01@gmail.com>
2024-07-16 02:30:00 -0400

    improve script to only display battery percentage if batteries exist

--- scripts/update_statusbar.sh
+++ scripts/update_statusbar.sh
@@ -3,39 +3,52 @@
 # script to update dwm's statusbar
 
 # various system information is pulled and stored in the following
-# variables. Those variables are then used to build the statusbar
-# in the xsetroot call.
+# variables. Those variables are added to the root_str string which
+# gets handed to xsetroot to set the statusbar.
 
-# volume level
-vol=$( pactl get-sink-volume @DEFAULT_SINK@ | grep --only-matching -P '\d*%' | head -1 )
-
-# date
-date=$( date +"%m-%d-%Y" )
-
-# time
-time=$( date +"%I:%M%p" )
-
-# check for batteries
-bat0=$( cat /sys/class/power_supply/BAT0/capacity )
-bat1=$( cat /sys/class/power_supply/BAT1/capacity )
-
-bat0=/sys/class/power_supply/BAT0
-bat1=/sys/class/power_supply/BAT1
-
-if [ -d "$bat0" ]; then
-    # directory exists
-else
-    # directory doesn't exist
-fi
+root_str=""
 
 # cpu utilization
 cpu_util=$(top -bn1 | grep "Cpu(s)" | \
     sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
     awk '{print 100 - $1}')
 
+root_str+="[CPU:$cpu_util%] "
+
 # memory utilization
 mem_util=$(free | head -n 2 | tail -n 1 | \
     awk '{printf("%.1f", ($3/$2)*100)}')
 
-xsetroot -name "[CPU:$cpu_util%] [MEM:$mem_util%] [VOL:$vol] [BAT:$bat0%] [$time]"
+root_str+="[MEM:$mem_util%] "
+
+# volume level
+vol=$( pactl get-sink-volume @DEFAULT_SINK@ | grep --only-matching -P '\d*%' | head -1 )
+
+root_str+="[VOL:$vol%] "
+
+# date
+date=$( date +"%m-%d-%Y" )
+
+# check for batteries
+
+bat0=/sys/class/power_supply/BAT0
+if [ -d "$bat0" ]; then
+    # directory exists
+    bat0_pow=$( cat /sys/class/power_supply/BAT0/capacity )
+    root_str+="[BAT0:$bat0_pow%] "
+fi
+
+bat1=/sys/class/power_supply/BAT1
+if [ -d "$bat0" ]; then
+    # directory exists
+    bat1_pow=$( cat /sys/class/power_supply/BAT1/capacity )
+    root_str+="[BAT1:$bat1_pow%] "
+fi
+
+# time
+time=$( date +"%I:%M%p" )
+
+root_str+="[$time]"
+
+xsetroot -name "$root_str"
 
