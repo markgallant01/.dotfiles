d6885b6913ed5eb02d440458d2bd79b89ac6d31f
markgallant01 <markgallant01@gmail.com>
2024-07-16 01:31:17 -0400

    add logic to check for batteries

--- scripts/update_statusbar.sh
+++ scripts/update_statusbar.sh
@@ -15,9 +15,18 @@ date=$( date +"%m-%d-%Y" )
 # time
 time=$( date +"%I:%M%p" )
 
-# battery info
+# check for batteries
 bat0=$( cat /sys/class/power_supply/BAT0/capacity )
 bat1=$( cat /sys/class/power_supply/BAT1/capacity )
+
+bat0=/sys/class/power_supply/BAT0
+bat1=/sys/class/power_supply/BAT1
+
+if [ -d "$bat0" ]; then
+    # directory exists
+else
+    # directory doesn't exist
+fi
 
 # cpu utilization
 cpu_util=$(top -bn1 | grep "Cpu(s)" | \
