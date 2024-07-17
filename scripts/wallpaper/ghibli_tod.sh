fbba6bc967f3c8870575325417b330c3e3722390
markgallant01 <markgallant01@gmail.com>
2024-07-17 19:33:13 -0400

    clean up

--- scripts/wallpaper/ghibli_tod.sh
+++ scripts/wallpaper/ghibli_tod.sh
@@ -3,20 +3,18 @@
 # grab the current hour
 hour=$(date +%H)
 
-# note: the hours will come back as '05' for 5am, with a leading zero
-# for any hour before 10am and after 12:59am. The leading zero makes
-# bash interpret it as an octal number. Prefixing with '10#' tells bash
-# to interpret the numbers as base 10.
+# trim leading zero
+hour=$(( 10#$hour ))
 
 # if between 4am and 4:59pm (04:00 - 16:59)
-if [[ $((10#"$hour")) -gt 3 ]] && [[ $((10#"$hour")) -lt 17 ]]; then
+if [[ $hour -gt 3 ]] && [[ $hour -lt 17 ]]; then
     # grab a random day time studio ghibli image
     image=$(find $HOME/Pictures/Wallpaper/new_ghibli_testing/Day/ \
         -mindepth 1 | shuf -n 1)
 fi
 
 # if between 5pm and 3:59am (17:00 - 03:59)
-if [[ $((10#"$hour")) -gt 16 ]] || [[ $(("$hour")) -lt 4 ]]; then
+if [[ $hour -gt 16 ]] || [[ $hour -lt 4 ]]; then
     # grab a random night time studio ghibli image
     image=$(find $HOME/Pictures/Wallpaper/new_ghibli_testing/Night/ \
         -mindepth 1 | shuf -n 1)
