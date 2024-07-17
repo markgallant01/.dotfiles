2392406df5de8aa156fa824778e5ead5710e1cf2
markgallant01 <markgallant01@gmail.com>
2024-07-16 21:36:22 -0400

    formatting fixes

--- scripts/wallpaper/ghibli_tod.sh
+++ scripts/wallpaper/ghibli_tod.sh
@@ -11,13 +11,15 @@ hour=$(date +%H)
 # if between 4am and 4:59pm (04:00 - 16:59)
 if [[ $((10#"$hour")) -gt 3 ]] && [[ $((10#"$hour")) -lt 17 ]]; then
     # grab a random day time studio ghibli image
-    image=$(find /home/mark/Pictures/Wallpaper/new_ghibli_testing/Day/ -mindepth 1 | shuf -n 1)
+    image=$(find $HOME/Pictures/Wallpaper/new_ghibli_testing/Day/ \
+        -mindepth 1 | shuf -n 1)
 fi
 
 # if between 5pm and 3:59am (17:00 - 03:59)
 if [[ $((10#"$hour")) -gt 16 ]] || [[ $(("$hour")) -lt 4 ]]; then
     # grab a random night time studio ghibli image
-    image=$(find /home/mark/Pictures/Wallpaper/new_ghibli_testing/Night/ -mindepth 1 | shuf -n 1)
+    image=$(find $HOME/Pictures/Wallpaper/new_ghibli_testing/Night/ \
+        -mindepth 1 | shuf -n 1)
 fi
 
 echo $image
