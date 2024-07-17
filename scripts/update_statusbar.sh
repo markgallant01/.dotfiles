68f323db426fee42b7c8c7638c993ffc02b30159
markgallant01 <markgallant01@gmail.com>
2024-07-17 03:57:09 -0400

    fix padding for CPU utilization so the digit before the decimal has room to expand

--- scripts/update_statusbar.sh
+++ scripts/update_statusbar.sh
@@ -11,7 +11,7 @@ root_str=""
 # cpu utilization
 cpu_util=$(top -bn1 | grep "Cpu(s)" | \
     sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
-    awk '{printf("%.1f", (100 - $1))}')
+    awk '{printf("% 4.1f", (100 - $1))}')
 
 root_str+="[CPU:$cpu_util%] "
 
