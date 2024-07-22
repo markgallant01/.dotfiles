878c0a5f5cf46be9290126461f31e634d5ea9980
markgallant01 <markgallant01@gmail.com>
2024-07-22 00:57:57 -0400

    lower brightness step for brightness control scripts

--- scripts/inc_brightness.sh
+++ scripts/inc_brightness.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
-brightness=$(( $brightness + 6400 ))
+brightness=$(( $brightness + 3200 ))
 echo $brightness > /sys/class/backlight/intel_backlight/brightness
 
