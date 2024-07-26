3f83908931b0bb7a96f4551e541ec4a90a986c2f
markgallant01 <markgallant01@gmail.com>
2024-07-26 06:31:50 -0400

    remove dumb plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -55,7 +55,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: 'opts = {}' is the same as calling 'require('fidget').setup({})'
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      --{ 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
     },
   },
 
