60da2607d17aea93b2abb64bd21c4fa7bac87cbc
markgallant01 <markgallant01@gmail.com>
2024-07-26 08:11:43 -0400

    remove java from installation list

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -13,7 +13,7 @@ declare -a packages=()
 packages+=("xfsprogs")
 
 # development tools
-packages+=("base-devel" "clang" "gdb" "npm" "neovim" "jdk-openjdk")
+packages+=("base-devel" "clang" "gdb" "npm" "neovim")
 
 # CLI tools
 packages+=("udisks2" "fastfetch" "zip" "unzip" "yt-dlp" "feh")
