51a2559b8213f1ce89df48c06de8c2320dc86608
markgallant01 <markgallant01@gmail.com>
2024-09-05 23:04:15 -0400

    update script to work with new directory path

--- scripts/wallpaper/ghibli_tod.sh
+++ scripts/wallpaper/ghibli_tod.sh
@@ -9,14 +9,14 @@ hour=$(( 10#$hour ))
 # if between 4am and 4:59pm (04:00 - 16:59)
 if [[ $hour -gt 3 ]] && [[ $hour -lt 17 ]]; then
     # grab a random day time studio ghibli image
-    image=$(find $HOME/Pictures/Wallpaper/new_ghibli_testing/Day/ \
+    image=$(find $HOME/Pictures/Wallpaper/PC/new_ghibli_testing/Day/ \
         -mindepth 1 | shuf -n 1)
 fi
 
 # if between 5pm and 3:59am (17:00 - 03:59)
 if [[ $hour -gt 16 ]] || [[ $hour -lt 4 ]]; then
     # grab a random night time studio ghibli image
-    image=$(find $HOME/Pictures/Wallpaper/new_ghibli_testing/Night/ \
+    image=$(find $HOME/Pictures/Wallpaper/PC/new_ghibli_testing/Night/ \
         -mindepth 1 | shuf -n 1)
 fi
 
