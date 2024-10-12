c030e63ea27dba7c40192d8bcca591e027b345f5
markgallant01 <markgallant01@gmail.com>
2024-10-12 19:11:43 -0400

    update brightness scripts to work properly with thinkpad t450s

--- scripts/inc_brightness.sh
+++ scripts/inc_brightness.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+hostname=$(cat /etc/hostname)
 brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
-brightness=$(( $brightness + 3200 ))
+
+if [[ "$hostname" == "archThinkpad" ]]; then
+    brightness=$(( $brightness + 71 ))
+else
+    brightness=$(( $brightness + 3200 ))
+fi
+
 echo $brightness > /sys/class/backlight/intel_backlight/brightness
 
