3a6749a3173c60593b1b3aa7201ac3c60fd6f04c
markgallant01 <markgallant01@gmail.com>
2025-05-22 04:29:16 -0400

    create config file for vim options

---
+++ nvim/lua/vim_options.lua
@@ -0,0 +1,50 @@
+-- ]]
+-- Set <space> as the leader key
+-- See ':help mapleader'
+-- NOTE: Must happen before plugins are required (otherwise wrong
+-- leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.guicursor = ""
+vim.o.mouse = 'a'
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.wo.nu = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.smartindent = true
+vim.o.breakindent = true
+
+vim.o.wrap = false
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+vim.o.termguicolors = true
+
+vim.o.scrolloff = 8
+vim.o.signcolumn = "yes"
+
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+vim.o.colorcolumn = "73"
+-- vim.o.foldmethod = 'indent'
+
+-- disable automatic commenting newline
+vim.cmd("autocmd FileType * set formatoptions-=ro")
+
+-- open all folds by default
+vim.cmd("autocmd BufRead * normal zR")
+
