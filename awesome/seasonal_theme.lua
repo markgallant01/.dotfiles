874785f1b8f81ec46bb708543cd0877cd66e07a8
markgallant01 <markgallant01@gmail.com>
2025-05-29 16:00:26 -0400

    store reference to wallpaper function rather than accepting a filepath directly

--- awesome/seasonal_theme.lua
+++ awesome/seasonal_theme.lua
@@ -4,7 +4,6 @@ local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
-local wall = require('seasonal')
 
 local theme = {}
 
@@ -98,7 +97,7 @@ theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
 -- generate path to chosen wallpaper and set it
-theme.wallpaper = wall.determineWallpaper().img_path
+theme.wallpaper = require('seasonal')
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
