41933d9efd8036aba7b21088a34937fafa2919bc
markgallant01 <markgallant01@gmail.com>
2025-06-03 22:53:09 -0400

    add margin to top bar

--- qtile/config.py
+++ qtile/config.py
@@ -167,6 +167,7 @@ screens = [
                 widget.Clock(format="%a %I:%M %p"),
             ],
             32,
+            margin=[10,15,0,15],
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  
             # Borders are magenta
