1f0b3c18827b542008a3b0ef9241362ab2240a3c
markgallant01 <markgallant01@gmail.com>
2025-07-17 15:26:19 -0400

    fix issue with building fzf

--- nvim/init.lua
+++ nvim/init.lua
@@ -63,9 +63,7 @@ require('lazy').setup({
       { 'nvim-lua/plenary.nvim' },
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild \
-        -DCMAKE_BUILD_TYPE=Release && \
-        cmake --build build --config Release'
+        build = 'make'
       },
       {
         'nvim-tree/nvim-web-devicons',
