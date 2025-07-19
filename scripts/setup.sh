dbd1796f4c5616c8e705eb659369d7f250304ea2
markgallant01 <markgallant01@gmail.com>
2025-07-19 15:56:54 -0400

    add chaotic-aur info and reflector call to script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,27 @@
 # echo on
 set -x
 
+# enable chaotic AUR
+# retrieve primary key
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+# install keyring and mirrorlist packages
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+# enable chaotic-aur in pacman.conf
+echo "" | sudo tee -a /etc/pacman.conf >/dev/null
+echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf >/dev/null
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf > /dev/null
+echo "" | sudo tee -a /etc/pacman.conf >/dev/null
+
+# run pacman once to generate and sync databases
+sudo pacman -Syu
+
+# run reflector once to get fast up to date mirrors
+sudo reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
+
 # call install script to install system packages
 ~/.dotfiles/scripts/install_packages.sh
 
@@ -21,15 +42,14 @@ git config --global color.ui auto
 git config --global pull.rebase false
 
 # symlink settings files
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
-ln -sf ~/.dotfiles/.xinitrc ~/.xinitrc
-ln -sf ~/.dotfiles/.Xresources ~/.Xresources
-ln -sf ~/.dotfiles/.fehbg ~/.fehbg
-ln -sf ~/.dotfiles/nvim/ ~/.config/nvim
+ln -sf ~/.dotfiles/alacritty/ ~/.config/alacritty
 ln -sf ~/.dotfiles/awesome/ ~/.config/awesome
-ln -sf ~/.dotfiles/ssh_config ~/.ssh/config
-ln -sf ~/.dotfiles/picom.conf ~/.config/picom.conf
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/gtk-3.0/ ~/.config/gtk-3.0
+ln -sf ~/.dotfiles/nvim/ ~/.config/nvim
+ln -sf ~/.dotfiles/picom.conf ~/.config/picom.conf
+ln -sf ~/.dotfiles/ssh_config ~/.ssh/config
+ln -sf ~/.dotfiles/.xinitrc ~/.xinitrc
 
 # copy conf files to appropriate folders
 # pacman hooks
