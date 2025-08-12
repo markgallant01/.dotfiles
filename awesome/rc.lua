f4239dfbbbf90fe3784732aa0bad566d59d911b0
markgallant01 <markgallant01@gmail.com>
2025-08-11 23:52:14 -0400

    enable default statusbar

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -113,75 +113,56 @@ awful.screen.connect_for_each_screen(function(s)
   set_wallpaper(s)
 
   -- Each screen has its own tag table.
-  awful.tag({ "1", "2", "3", "4", "5", "6" },
+  awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" },
     s, awful.layout.layouts[1]
   )
 
   -- Create a promptbox for each screen
   s.mypromptbox = awful.widget.prompt()
-
+  -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+  -- We need one layoutbox per screen.
+  s.mylayoutbox = awful.widget.layoutbox(s)
+  s.mylayoutbox:buttons(gears.table.join(
+                         awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                         awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                         awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                         awful.button({ }, 5, function () awful.layout.inc(-1) end)))
   -- Create a taglist widget
   s.mytaglist = awful.widget.taglist {
-    screen  = s,
-    filter  = awful.widget.taglist.filter.all,
-    buttons = taglist_buttons
+      screen  = s,
+      filter  = awful.widget.taglist.filter.all,
+      buttons = taglist_buttons
   }
 
   -- Create a tasklist widget
   s.mytasklist = awful.widget.tasklist {
-    screen  = s,
-    filter  = awful.widget.tasklist.filter.currenttags
+      screen  = s,
+      filter  = awful.widget.tasklist.filter.currenttags,
+      buttons = tasklist_buttons
   }
-
-  -- Create a systray widget
-  s.mysystray = wibox.widget.systray()
-
-  -- Create a textclock widget
-  s.mytextclock = wibox.widget.textclock("%_I:%M%P")
-  s.textdate = wibox.widget.textclock("%_d %b, %Y")
-
-  -- Create an imagebox widget which will contain an icon indicating
-  -- which layout we're using. We need one layoutbox per screen.
-  s.mylayoutbox = awful.widget.layoutbox(s)
-  s.mylayoutbox:buttons(gears.table.join(
-    awful.button({ }, 1, function () awful.layout.inc( 1) end),
-    awful.button({ }, 3, function () awful.layout.inc(-1) end)
-  ))
-
   -- Create the wibox
---[[  s.mywibox = awful.wibar({ position = "top", screen = s })
-  s.mywibox.border_width = 0
-  s.mywibox.border_color = "#ffffff00"
-  s.mywibox.opacity = 1
---]]
-  local left_panel = wibox.widget({
-    s.mytaglist,
-    s.mylayoutbox,
-    layout = wibox.layout.align.horizontal
-  })
-  left_panel.opacity = 1
-
-  local middle_panel = wibox.widget {
-    s.textdate,
-    layout = wibox.layout.align.horizontal
-  }
-
-  local right_panel = wibox.widget {
-    s.mysystray,
-    s.mytextclock,
-    layout = wibox.layout.align.horizontal
-  }
+  s.mywibox = awful.wibar({ position = "top", screen = s })
 
   -- Add widgets to the wibox
-  --s.mywibox:setup(left_panel)
-  s.leeeyout = wibox.layout.align.horizontal()
-  s.leeeyout.expand = 'none'
-  s.leeeyout.first = left_panel
-  s.leeeyout.second = middle_panel
-  s.leeeyout.third = right_panel
---  s.mywibox.widget = s.leeeyout
-
+  s.mywibox:setup {
+      layout = wibox.layout.align.horizontal,
+      { -- Left widgets
+          layout = wibox.layout.fixed.horizontal,
+          mylauncher,
+          s.mytaglist,
+          s.mypromptbox,
+      },
+      s.mytasklist, -- Middle widget
+      { -- Right widgets
+          layout = wibox.layout.fixed.horizontal,
+          mykeyboardlayout,
+          wibox.widget.systray(),
+          mytextclock,
+          s.mylayoutbox,
+      },
+  }
 end)
+
 -- helper function centers mouse on client window
 local function centerMouseOnClient(c)
   mouse.coords {
