55e04ca68ab9910c763bc0da14a7ff3b6a097bf5
markgallant01 <markgallant01@gmail.com>
2025-08-22 14:44:40 -0400

    update function to not automatically switch groups when moving windows to new groups

--- qtile/config.py
+++ qtile/config.py
@@ -105,17 +105,17 @@ for i in groups:
             lazy.group[i.name].toscreen(),
             desc=f"Switch to group {i.name}"),
 
+        # mod + shift + group number = move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            desc="move focused window to group {}".format(i.name)),
+
+        # Or, use below if you prefer to move & switch to that group.
         # mod + shift + group number = switch to & move focused
         # window to group
-        Key([mod, "shift"],
-            i.name,
-            lazy.window.togroup(i.name, switch_group=True),
-            desc=f"Switch to & move focused window to group {i.name}"),
-
-        # Or, use below if you prefer not to switch to that group.
-        # # mod + shift + group number = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
+        #Key([mod, "shift"],
+        #    i.name,
+        #    lazy.window.togroup(i.name, switch_group=True),
+        #    desc=f"Switch to & move focused window to group {i.name}"),
     ])
 
 layouts = [
