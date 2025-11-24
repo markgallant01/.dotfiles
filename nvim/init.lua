5316a70da25bd84da30f6d088e69afd839b2a260
markgallant01 <markgallant01@gmail.com>
2025-11-24 00:00:05 -0500

    enable block cursor in all modes

--- nvim/init.lua
+++ nvim/init.lua
@@ -166,6 +166,9 @@ vim.o.inccommand = 'split'
 -- Show which line your cursor is on
 vim.o.cursorline = false
 
+-- Block cursor
+vim.o.guicursor = 'n-v-c-i:block'
+
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
