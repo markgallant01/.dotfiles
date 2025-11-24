e3fb5474d6e8fbaf01bd65e3f643670d193b265b
markgallant01 <markgallant01@gmail.com>
2025-11-23 23:57:07 -0500

    revert to tokyonight

--- nvim/init.lua
+++ nvim/init.lua
@@ -863,11 +863,11 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'craftzdog/solarized-osaka.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('solarized-osaka').setup {
+      require('tokyonight').setup {
         transparent = false,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
@@ -877,7 +877,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'solarized-osaka'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
