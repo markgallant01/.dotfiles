dc70ab20680ddcc9e64e8828d93f22be0410963c
markgallant01 <markgallant01@gmail.com>
2026-04-18 01:40:59 -0400

    disable colorscheme transparency

--- nvim/init.lua
+++ nvim/init.lua
@@ -45,7 +45,7 @@ vim.pack.add({
 require("solarized-osaka").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  transparent = true, -- Enable this to disable setting the background color
+  transparent = false, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
   styles = {
     -- Style to be applied to different syntax groups
