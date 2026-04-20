e344d48d748372bd820d989f7338da5fbca590c6
markgallant01 <markgallant01@gmail.com>
2026-04-20 01:23:40 -0400

    formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -68,10 +68,11 @@ vim.lsp.config('lua_ls', {
 
 -- setup colorscheme
 require("solarized-osaka").setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  transparent = false, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+  -- Enable this to disable setting the background color
+  transparent = false, 
+  -- Configure the colors used when opening a `:terminal` in
+  -- [Neovim](https://github.com/neovim/neovim)
+  terminal_colors = true,
   styles = {
     -- Style to be applied to different syntax groups
     -- Value is any valid attr-list value for `:help nvim_set_hl`
@@ -83,14 +84,20 @@ require("solarized-osaka").setup({
     sidebars = "dark", -- style for sidebars, see below
     floats = "dark", -- style for floating windows
   },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  -- Set a darker background on sidebar-like windows. For example:
+  -- `["qf", "vista_kind", "terminal", "packer"]`
+  -- Adjusts the brightness of the colors of the **Day** style.
+  -- Number between 0 and 1, from dull to vibrant colors
+  sidebars = { "qf", "help" },  day_brightness = 0.3,
+  -- Enabling this option, will hide inactive statuslines and replace them
+  -- with a thin border instead. Should work with the standard **StatusLine**
+  -- and **LuaLine**.
+  hide_inactive_statusline = false,
   dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
+  -- When `true`, section headers in the lualine theme will be bold
+  lualine_bold = false,
+  --- You can override specific color groups to use other groups or a hex
+  --- color function will be called with a ColorScheme table
   ---@param colors ColorScheme
   on_colors = function(colors) end,
 
