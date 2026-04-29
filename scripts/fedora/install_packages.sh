9e7ecec91fc0e3074530c48319ea2a395f6e8b68
markgallant01 <markgallant01@gmail.com>
2026-04-29 17:59:18 -0400

    flesh out scripts based on the arch ones

---
+++ scripts/fedora/install_packages.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# echo on
+set -x
+
+# this script builds an array of packages section-by-section
+# and then calls pacman to install them all together at the end.
+
+# array to store package list
+declare -a packages=()
+
+# wifi drivers (add more for other wifi cards)
+packages+=("iwlwifi-mvm-firmware")
+
+# window manager and shell
+packages+=("niri" "dms")
+
+# niri deps
+packages+=("polkit-kde")
+
+# DMS integrations
+packages+=("khal" "cups-pk-helper" "power-profiles-daemon")
+
+# terminal
+packages+=("foot")
+
+# CLI tools
+packages+=("pandoc" "btop" "yt-dlp" "tldr" "texinfo" "fastfetch")
+
+# VPN port-forward script dep
+packages+=("libnatpmp")
+
+# Proton VPN frontend
+packages+=("proton-vpn-gnome-desktop")
+
+# neovim deps
+packages+=("fd-find")
+
+# development tools
+packages+=("cmake" "gdb" "java-latest-openjdk-devel.x86_64")
+packages+=("docker" "docker-compose" "uv")
+
+# graphical frontends
+packages+=("pavucontrol")
+
+# gaming stuff
+packages+=("gamescope" "steam")
+
+# multimedia
+packages+=("qbittorrent" "vlc")
+
+# communication
+packages+=("discord")
+
+# web browsers
+packages+=("firefox")
+
+# misc utilities
+packages+=("xdg-user-dirs")
+
+# video drivers depend on GPU manufacturer:
+gpu_option=0
+while [[ "$gpu_option" != 1 ]] &&
+      [[ "$gpu_option" != 2 ]] &&
+      [[ "$gpu_option" != 3 ]]
+do
+  echo "Choose GPU type: [1] Intel" #, [2] AMD, [3] Nvidia..."
+  read gpu_option
+done
+
+# intel iGPU:
+if [[ "$gpu_option" == 1 ]]; then
+    packages+=("intel-media-driver")
+fi
+
+# AMD CPU GPU? --todo
+#if [[ "$option" == 2]]; then
+#fi
+
+# nvidia:
+#if [[ "$gpu_option" == 3 ]]; then
+#    packages+=()
+#fi
+
+# install full package list. --exclude=alacritty skips the weak dependency
+# for niri that installs alacritty by default. we dont need it since we have
+# foot.
+sudo dnf install "${packages[@]}" --exclude=alacritty
+
+# install DMS greeter
+dms greeter install
+
---
+++ scripts/fedora/setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# echo on
+set -x
+
+# set up repos
+# enable DMS COPR repo:
+sudo dnf copr enable avengemedia/dms
+
+# enable RPM Fusion repos for discord and steam:
+# free repo:
+sudo dnf install \
+    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+# non-free repo:
+sudo dnf install \
+    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# config for steam:
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+
+# proton-vpn
+# download the package with the repo info and keys:
+wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
+
+# install proton-vpn repo:
+sudo dnf install \
+    ./protonvpn-stable-release-1.0.3-1.noarch.rpm && sudo dnf check-update --refresh
+
+# call install script to install system packages
+~/.dotfiles/scripts/fedora/install_packages.sh
+
+# create xdg-user-dirs
+xdg-user-dirs-update
+
+# create some more directories
+mkdir ~/Pictures/Screenshots
+mkdir ~/Trash
+mkdir ~/.config
+mkdir ~/.ssh
+
+# configure git
+git config --global user.name markgallant01
+git config --global user.email markgallant01@gmail.com
+git config --global init.defaultBranch main
+git config --global color.ui auto
+git config --global pull.rebase false
+
+# symlink settings files
+ln -sf ~/.dotfiles/.bashrc      ~/.bashrc
+ln -sf ~/.dotfiles/gtk-3.0/     ~/.config/gtk-3.0
+ln -sf ~/.dotfiles/nvim/        ~/.config/nvim
+ln -sf ~/.dotfiles/ssh_config   ~/.ssh/config
+ln -sf ~/.dotfiles/niri         ~/.config/niri
+ln -sf ~/.dotfiles/foot         ~/.config/foot
+
+# enable DMS service
+systemctl --user enable dms
+
+# generate new ssh keys for github
+ssh-keygen -t ed25519 -C markgallant01@gmail.com
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
