5e3855d1b791fc5f7e6626f7b8d4bf67c5996486
markgallant01 <markgallant01@gmail.com>
2026-04-29 19:03:39 -0400

    fix output file name

--- notes/make_doc.sh
+++ notes/make_doc.sh
@@ -5,6 +5,6 @@ if [[ "$#" != 1 ]]; then
     echo "Incorrect number of args."
     echo "Usage: make_doc [arch/fedora/gentoo]"
 else
-    pandoc -s ./"$1"/"$1"_install_notes.md -o ./"$1"/"$"_install_notes.html
+    pandoc -s ./"$1"/"$1"_install_notes.md -o ./"$1"/"$1"_install_notes.html
 fi
 
