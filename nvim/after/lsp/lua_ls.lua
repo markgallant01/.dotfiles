8041f63edbc33924f85f9264ed9616afc50a62e5
markgallant01 <markgallant01@gmail.com>
2026-05-02 16:01:50 -0400

    add some comments to the lua_ls lsp config file

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -1,7 +1,10 @@
+-- these settings get automatically applied to the "lua_ls" LSP
 vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
       diagnostics = {
+        -- warn the LSP disgnostic about the undefined global "vim" so that
+        -- we don't see a million warnings in our vim config files
         globals = { "vim" }
       }
     }
