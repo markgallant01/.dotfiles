a1853a3b7e88c8f246210d2285a2946c30023278
markgallant01 <markgallant01@gmail.com>
2026-05-02 02:02:37 -0400

    task: start simplifying init.lua by moving plugin configs to their own files. set up lazy package manager and initialize it. move vim options to their own file and initialize them.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,133 +1,53 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.g.have_nerd_font = true
-
-vim.o.number = true
-vim.o.relativenumber = true
-
-vim.o.mouse = 'a'
-vim.o.showmode = false
-
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
-
-vim.o.breakindent = true
-
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-
-vim.o.undofile = true
-
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
-vim.o.signcolumn = 'yes'
-vim.o.colorcolumn = '80'
-
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
-vim.o.cursorline = true
-vim.o.guicursor = 'n-v-c-i:block'
-
-vim.o.scrolloff = 10
-vim.o.wrap = false
-
-vim.o.filetype = on
+require('config.vim_opts')
+require('config.lazy')
 
 -- plugins
-vim.pack.add({
-    'https://github.com/neovim/nvim-lspconfig',
-    'https://github.com/craftzdog/solarized-osaka.nvim',
-    'https://github.com/windwp/nvim-autopairs',
-})
+--vim.pack.add({
+--    'https://github.com/neovim/nvim-lspconfig',
+--    'https://github.com/windwp/nvim-autopairs',
+--})
 
 -- enable LSPs
-vim.lsp.enable('bashls')
-vim.lsp.enable('clangd')
-vim.lsp.enable('cssls')
-vim.lsp.enable('denols')
-vim.lsp.enable('dockerls')
-vim.lsp.enable('html')
-vim.lsp.enable('jsonls')
-vim.lsp.enable('lua_ls')
-vim.lsp.enable('pyright')
+--vim.lsp.enable('bashls')
+--vim.lsp.enable('clangd')
+--vim.lsp.enable('cssls')
+--vim.lsp.enable('denols')
+--vim.lsp.enable('dockerls')
+--vim.lsp.enable('html')
+--vim.lsp.enable('jsonls')
+--vim.lsp.enable('lua_ls')
+--vim.lsp.enable('pyright')
 
 -- extra config for LSPs
-vim.lsp.config('lua_ls', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = {'vim'}
-            }
-        }
-    }
-})
+--vim.lsp.config('lua_ls', {
+--    settings = {
+--        Lua = {
+--            diagnostics = {
+--                globals = {'vim'}
+--            }
+--        }
+--    }
+--})
 
-require('nvim-autopairs').setup({
-  enabled = function(bufnr) return true end, -- control if auto-pairs should be enabled when attaching to a buffer
-  disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
-  disable_in_macro = true, -- disable when recording or executing a macro
-  disable_in_visualblock = false, -- disable when insert after visual block mode
-  disable_in_replace_mode = true,
-  ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
-  enable_moveright = true,
-  enable_afterquote = true, -- add bracket pairs after quote
-  enable_check_bracket_line = true, --- check bracket in same line
-  enable_bracket_in_quote = true, --
-  enable_abbr = false, -- trigger abbreviation
-  break_undo = true, -- switch for basic rule break undo sequence
-  check_ts = false,
-  map_cr = true,
-  map_bs = true, -- map the <BS> key
-  map_c_h = false, -- Map the <C-h> key to delete a pair
-  map_c_w = false, -- map <c-w> to delete a pair if possible
-})
-
--- setup colorscheme
-require("solarized-osaka").setup({
-  -- Enable this to disable setting the background color
-  transparent = false, 
-  -- Configure the colors used when opening a `:terminal` in
-  -- [Neovim](https://github.com/neovim/neovim)
-  terminal_colors = true,
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
-  },
-  -- Set a darker background on sidebar-like windows. For example:
-  -- `["qf", "vista_kind", "terminal", "packer"]`
-  -- Adjusts the brightness of the colors of the **Day** style.
-  -- Number between 0 and 1, from dull to vibrant colors
-  sidebars = { "qf", "help" },  day_brightness = 0.3,
-  -- Enabling this option, will hide inactive statuslines and replace them
-  -- with a thin border instead. Should work with the standard **StatusLine**
-  -- and **LuaLine**.
-  hide_inactive_statusline = false,
-  dim_inactive = false, -- dims inactive windows
-  -- When `true`, section headers in the lualine theme will be bold
-  lualine_bold = false,
-  --- You can override specific color groups to use other groups or a hex
-  --- color function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
-})
+--require('nvim-autopairs').setup({
+--  enabled = function(bufnr) return true end, -- control if auto-pairs should be enabled when attaching to a buffer
+--  disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
+--  disable_in_macro = true, -- disable when recording or executing a macro
+--  disable_in_visualblock = false, -- disable when insert after visual block mode
+--  disable_in_replace_mode = true,
+--  ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+--  enable_moveright = true,
+--  enable_afterquote = true, -- add bracket pairs after quote
+--  enable_check_bracket_line = true, --- check bracket in same line
+--  enable_bracket_in_quote = true, --
+--  enable_abbr = false, -- trigger abbreviation
+--  break_undo = true, -- switch for basic rule break undo sequence
+--  check_ts = false,
+--  map_cr = true,
+--  map_bs = true, -- map the <BS> key
+--  map_c_h = false, -- Map the <C-h> key to delete a pair
+--  map_c_w = false, -- map <c-w> to delete a pair if possible
+--})
 
 vim.cmd[[colorscheme solarized-osaka]]
 
---
+++ nvim/lua/config/vim_opts.lua
@@ -0,0 +1,40 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.mouse = 'a'
+vim.o.showmode = false
+
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+vim.o.breakindent = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.signcolumn = 'yes'
+vim.o.colorcolumn = '80'
+
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+vim.o.cursorline = true
+vim.o.guicursor = 'n-v-c-i:block'
+
+vim.o.scrolloff = 10
+vim.o.wrap = false
+
+vim.o.filetype = on
+
