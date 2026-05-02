14b741014000a4247ff401a484c8004d353a66ec
markgallant01 <markgallant01@gmail.com>
2026-05-02 15:56:34 -0400

    add some helpful comments for future me to understand the init file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,16 @@
+-- neovim init file
+-- the entire setup is started from this file
+
+-- pull in vim options before anything else so that things like keybinds
+-- and leader keys are set for any plugins that might need them
 require('config.vim_opts')
+
+-- initialize lazy package manager. the package manager should then
+-- load up the plugins defined and configured in the /lua/plugins/ folder
 require('config.lazy')
 
+-- load colorscheme. this happens here instead of in the colorscheme's config
+-- file so that we could have multiple colorschemes installed and we can
+-- just pick whichever one we want to load here
 vim.cmd[[colorscheme solarized-osaka]]
 
