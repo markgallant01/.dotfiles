52cf0b0e3642747be957a30ff2ece75c66eb80d9
markgallant01 <markgallant01@gmail.com>
2026-05-02 18:21:03 -0400

    add kickstarts keybinds to plugin keybinds file in case we want to use them later

--- nvim/lua/config/plugin_keybinds.lua
+++ nvim/lua/config/plugin_keybinds.lua
@@ -2,6 +2,7 @@
 -- it will be loaded after lazy loads the plugins.
 
 -- treesitter
+-- See `:help telescope.builtin`
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files,
   { desc = 'Telescope find files' })
@@ -14,3 +15,19 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers,
 
 vim.keymap.set('n', '<leader>fh', builtin.help_tags,
   { desc = 'Telescope help tags' })
+
+-- these are kickstart's keybinds which we might prefer.
+--[[
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+--]]
+
