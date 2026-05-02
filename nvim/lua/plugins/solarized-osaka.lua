ef7a370b729bc3030bc0f308270f79fd96f5f574
markgallant01 <markgallant01@gmail.com>
2026-05-02 16:43:20 -0400

    clean up file formatting and add a description

--- nvim/lua/plugins/solarized-osaka.lua
+++ nvim/lua/plugins/solarized-osaka.lua
@@ -1,3 +1,4 @@
+-- configuration for solarized-osaka plugin
 return {
   "craftzdog/solarized-osaka.nvim",
   lazy = false,
@@ -5,8 +6,14 @@ return {
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    transparent = false, -- Enable this to disable setting the background color
-    terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+
+    -- Enable this to disable setting the background color
+    transparent = false,
+
+    -- Configure the colors used when opening a `:terminal` in
+    -- [Neovim](https://github.com/neovim/neovim)
+    terminal_colors = true,
+
     styles = {
       -- Style to be applied to different syntax groups
       -- Value is any valid attr-list value for `:help nvim_set_hl`
@@ -14,17 +21,32 @@ return {
       keywords = { italic = true },
       functions = {},
       variables = {},
+
       -- Background styles. Can be "dark", "transparent" or "normal"
       sidebars = "dark", -- style for sidebars, see below
       floats = "dark", -- style for floating windows
     },
-    sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-    hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = false, -- dims inactive windows
-    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
-    --- You can override specific color groups to use other groups or a hex color
+    -- Set a darker background on sidebar-like windows. For example:
+    -- `["qf", "vista_kind", "terminal", "packer"]`
+    sidebars = { "qf", "help" },
+
+    -- Adjusts the brightness of the colors of the **Day** style. Number
+    -- between 0 and 1, from dull to vibrant colors
+    day_brightness = 0.3,
+
+    -- Enabling this option, will hide inactive statuslines and replace them
+    -- with a thin border instead. Should work with the standard
+    -- **StatusLine** and **LuaLine**.
+    hide_inactive_statusline = false,
+
+    dim_inactive = false, -- dims inactive windows
+
+    -- When `true`, section headers in the lualine theme will be bold
+    lualine_bold = false,
+
+    --- You can override specific color groups to use other groups
+    --- or a hex color
     --- function will be called with a ColorScheme table
     ---@param colors ColorScheme
     on_colors = function(colors) end,
