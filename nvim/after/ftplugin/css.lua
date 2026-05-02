19e833270b0dd115ec17dd7455794c3144a4e1ff
markgallant01 <markgallant01@gmail.com>
2026-05-02 15:59:50 -0400

    add comment descriptions for ftplugin files

--- nvim/after/ftplugin/css.lua
+++ nvim/after/ftplugin/css.lua
@@ -1,3 +1,4 @@
+-- these settings get automatically set for css files
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
--- nvim/after/ftplugin/html.lua
+++ nvim/after/ftplugin/html.lua
@@ -1,3 +1,4 @@
+-- these settings get automatically set for html files
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
