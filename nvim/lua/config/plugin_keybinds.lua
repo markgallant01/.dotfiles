5df9b7bf448eb62b58b23708734e0f508b97965e
markgallant01 <markgallant01@gmail.com>
2026-05-03 01:45:56 -0400

    change telescope keybinds to be how they used to be because I keep hitting them by mistake

--- nvim/lua/config/plugin_keybinds.lua
+++ nvim/lua/config/plugin_keybinds.lua
@@ -4,16 +4,16 @@
 -- treesitter
 -- See `:help telescope.builtin`
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files,
-  { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files,
+  { desc = 'Telescope search files' })
 
-vim.keymap.set('n', '<leader>fg', builtin.live_grep,
+vim.keymap.set('n', '<leader>sg', builtin.live_grep,
   { desc = 'Telescope live grep' })
 
-vim.keymap.set('n', '<leader>fb', builtin.buffers,
+vim.keymap.set('n', '<leader>sb', builtin.buffers,
   { desc = 'Telescope buffers' })
 
-vim.keymap.set('n', '<leader>fh', builtin.help_tags,
+vim.keymap.set('n', '<leader>sh', builtin.help_tags,
   { desc = 'Telescope help tags' })
 
 -- these are kickstart's keybinds which we might prefer.
