91c31491641385dc23942aeee2f5af5134835c4f
markgallant01 <markgallant01@gmail.com>
2026-06-07 20:10:41 -0400

    configure lua_ls language server to disable lowercase-global warning

--- config/nvim/after/lsp/lua_ls.lua
+++ config/nvim/after/lsp/lua_ls.lua
@@ -3,6 +3,7 @@ vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
       diagnostics = {
+        disable = { "lowercase-global" },
         -- warn the LSP disgnostic about the undefined global "vim" so that
         -- we don't see a million warnings in our vim config files
         globals = { "vim" }
