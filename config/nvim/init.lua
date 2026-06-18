59c178dfd71aa512f5f39e15f4191789f700e8f2
markgallant01 <markgallant01@gmail.com>
2026-06-17 21:50:11 -0400

    modify some plugin settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -86,19 +86,6 @@ do
   vim.o.splitright = true
   vim.o.splitbelow = true
 
-  -- Sets how neovim will display certain whitespace characters in the
-  -- editor.
-  --  See `:help 'list'`
-  --  and `:help 'listchars'`
-  --
-  --  Notice listchars is set using `vim.opt` instead of `vim.o`.
-  --  It is very similar to `vim.o` but offers an interface for
-  --  conveniently interacting with tables.
-  --   See `:help lua-options`
-  --   and `:help lua-guide-options`
-  vim.o.list = true
-  vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
   -- Preview substitutions live, as you type!
   vim.o.inccommand = 'split'
 
@@ -703,46 +690,6 @@ do
       --  For example, in C this would take you to the header.
       map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-      -- The following two autocommands are used to highlight references
-      -- of the word under your cursor when your cursor rests there for
-      -- a little while.
-      --  See `:help CursorHold` for information about when this is executed
-      --
-      -- When you move your cursor, the highlights will be cleared
-      -- (the second autocommand).
-      local client = vim.lsp.get_client_by_id(event.data.client_id)
-      if client and client:supports_method(
-        'textDocument/documentHighlight',
-        event.buf
-      ) then
-        local highlight_augroup = vim.api.nvim_create_augroup(
-          'kickstart-lsp-highlight',
-          { clear = false }
-        )
-        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-          buffer = event.buf,
-          group = highlight_augroup,
-          callback = vim.lsp.buf.document_highlight,
-        })
-
-        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-          buffer = event.buf,
-          group = highlight_augroup,
-          callback = vim.lsp.buf.clear_references,
-        })
-
-        vim.api.nvim_create_autocmd('LspDetach', {
-          group = vim.api.nvim_create_augroup('kickstart-lsp-detach',
-            { clear = true }),
-          callback = function(event2)
-            vim.lsp.buf.clear_references()
-            vim.api.nvim_clear_autocmds({
-              group = 'kickstart-lsp-highlight',
-              buffer = event2.buf
-            })
-          end,
-        })
-      end
 
       -- The following code creates a keymap to toggle inlay hints in your
       -- code, if the language server you are using supports them
@@ -1107,7 +1054,12 @@ require("nvim-autopairs").setup({})
 
 
 vim.pack.add({ gh("lukas-reineke/indent-blankline.nvim") })
-require("ibl").setup({})
+require("ibl").setup({
+  scope = {
+    show_start = false,
+    show_end = false
+  }
+})
 
 
 vim.pack.add({ gh("nvim-lualine/lualine.nvim") })
