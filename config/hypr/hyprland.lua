9a788de3e4dc026dc51bd173dafd26b3f2853dae
markgallant01 <markgallant01@gmail.com>
2026-09-12 20:35:36 -0400

    feat(config/hyprland): create keybind for screenshot utility

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -186,6 +186,9 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
+-- screenshots
+hl.bind(mainMod .. " + s", hl.dsp.exec_cmd('grim -g "$(slurp)" - | swappy -f -'))
+
 -- enable this to swap capslock and ctrl. figure out how this works with
 -- other keyboard
 --hl.config({
